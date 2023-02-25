d99c47f349493c8ce29e40a203320fe01b96d369
Mark Owings <owingsmk@mail.uc.edu>
2023-02-25 13:26:09 -0500

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,13 @@ public class Main {
     public static void main(String[] args) {
 
         for(int i =0; i < 5; i++){
-            while(i >= 0) {
-                System.out.println("*");
+            System.out.println("*");
+            while (i >= 0) {
+                System.out.print(i);
                 i = i + 1;
             }
         }
+
 
 
     }
