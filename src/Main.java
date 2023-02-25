bf382f4ac5322a1b2dfc4d5e6ebe7545e460da4a
Mark Owings <owingsmk@mail.uc.edu>
2023-02-25 14:00:32 -0500

    Third commit starting over

--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        String symbol = "";
 
-        //System.out.println("Enter # of rows ");
-        //rows = scanner.nextInt();
-        //System.out.println("Enter # of columns: ");
-        //columns = scanner.nextInt();
-        System.out.println("Enter Symbol to use: ");
-        symbol = scanner.next();
 
-        for (int i=1; i<=1; i++){
-            System.out.println();
-            while( i<=0){
-                System.out.println(i);
-                i = i + 1;
-            }
-            for(int j=1; j<=1; j++){
-                System.out.print(symbol + i++);
-            }
         }
-
-
-
-
-    }
-}
